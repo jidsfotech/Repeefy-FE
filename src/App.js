3bc63fc6a2ac13d8dcdf7042eb7e4c08981508b9
Daniel Bemsen <danireptor7@gmail.com>
2020-12-07 22:46:36 +0100

    routing app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import "./App.css";
+import NotFound from "./Pages/404/404";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 const App = () => {
   return (
     <div className="App">
-      App.js
       <Router>
         <Switch>
-          <Route path="/" />
-          <Route path="/users" />
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/404" component={NotFound} /> 
+          <Redirect to="/404" />
         </Switch>
       </Router>
     </div>
---
+++ src/Pages/404/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./404.css";
+
+const NotFound = (props) => {
+  return (
+    <div className="404">
+      Page not found
+    </div>
+  );
+};
+
+export default NotFound;
