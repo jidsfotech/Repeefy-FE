76591bb6371f76295674bcca382835a7924db0e3
Majeed Shuaib <jidsfotech@gmail.com>
2020-12-10 12:48:26 +0100

    Added Navbar to the dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,7 +3,37 @@ import "./Dashboard.css";
 import Wallet from "../Wallet/Wallet"
 
 const Dashboard = (props) => {
-  return <div className="Dashboard"><Wallet /></div>;
+  return (
+    <div className="Dashboard">
+      <div className="TopNav">
+        <nav className="DashboardNav">
+          <div className="BrandLogo">
+            <div
+              onClick={() => (window.location.href = "/")}
+            >
+              <img
+                src="https://res.cloudinary.com/repify/image/upload/v1602121150/RepifyLogo.png"
+                alt="Repify" />
+            </div>
+          </div>
+          <div className="UserProfile">
+            <img
+              src="https://res.cloudinary.com/repify/image/upload/v1607536594/notification-white_re8fgz.png"
+              alt="Notification"
+              className="Notify NavItem"
+            />
+            <img
+              src="https://res.cloudinary.com/repify/image/upload/v1603726604/profile-picture-placeholder.png"
+              alt="Profile"
+              className="UserImg NavItem"
+            />
+            <p className="UserName NavItem">User Name</p>
+          </div>
+        </nav>
+      </div>
+      <Wallet />
+    </div>
+  )
 };
 
 export default Dashboard;
