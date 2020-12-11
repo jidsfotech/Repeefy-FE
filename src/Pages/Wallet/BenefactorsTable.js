3f993b63128b7fd3783d6b5ad599467c2962115b
Majeed Shuaib <jidsfotech@gmail.com>
2020-12-11 22:03:22 +0100

    implented the Beneficiaries and Benefactors Table

---
+++ src/Pages/Wallet/BenefactorsTable.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+/** 
+ * Renders an empty state if the current user has no benefactor to display
+ * This component is returned when the benefactor object is epty
+ */
+const NoBenefactorFound = () => {
+    return (
+        <tr>
+            <td colSpan="4">
+                <div className="flex-wrapper">
+                    <div className="emptyInfo ">
+                        <img src="https://res.cloudinary.com/repify/image/upload/v1607708782/You_have_no_Benefactors.png" alt="No benefactors yet"></img>
+                    </div>
+                    <div className="emptyImg ">
+                        <img src="https://res.cloudinary.com/repify/image/upload/v1601862897/empty_state.svg" alt="no benefactor"></img>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    );
+};
+
+/** 
+ * Renders table rows 
+ * This component  recieves benefactor object as "props" and creates a table row for each element
+ * creates a table row for each benefactor
+ */
+const RenderTableRows = (props) => {
+    const benefactor = props.benefactor;
+    let statusIndicator = () => {
+        switch (benefactor.status) {
+            case "active":
+                return (<img src="https://res.cloudinary.com/repify/image/upload/v1603638053/active.png" alt="vector"></img>);
+            case "not-active":
+                return (<img src="https://res.cloudinary.com/repify/image/upload/v1603638053/Inactive.png" alt="vector"></img>);
+
+            default:
+                return (<img src="https://res.cloudinary.com/repify/image/upload/v1603638053/active.png" alt="vector"></img>);
+        }
+    };
+    return (
+        <tr className="benefactorsTableRows">
+            <td>{(statusIndicator())} {benefactor.benefactor_name}</td>
+            <td>{benefactor.benefactor_id}</td>
+            <td>{benefactor.amount}</td>
+            <td>{benefactor.date}</td>
+        </tr>
+    );
+};
+
+const RenderBenefactorsTable = (props) => {
+    const rows = [];
+
+    //return "NoBenefactorFound" component if "props === null". i.e if the current user has no benefactors
+    var tableRows = () => {
+        if (!props.benefactors) {
+            return <NoBenefactorFound />;
+        } else {
+            props.benefactors.forEach((benefactor) => {
+                rows.push(<RenderTableRows benefactor={benefactor} key={benefactor.benefactor_name} />);
+            });
+            return rows;
+        }
+    };
+    return (
+        <table className="benefactorsTable">
+            <thead>
+                <tr>
+                    <th> Name </th>
+                    <th> User ID </th>
+                    <th> Amount </th>
+                    <th> Date </th>
+                </tr>
+            </thead>
+            <tbody className="benefactorsTableBody">
+                {tableRows()}
+            </tbody>
+        </table>
+    );
+
+};
+
+export default RenderBenefactorsTable;
--- src/Pages/Wallet/Wallet.js
+++ src/Pages/Wallet/Wallet.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./Wallet.css";
-import WalletTable from "./WalletTable";
+import "./Benefactors.css";
+import "./Beneficiaries.css";
+import BenefactorsTable from "./BenefactorsTable";
+import BeneficiariesTable from "./BeneficiriesTable";
+import {
+  benefactorsMockData,
+  beneficiariesMockData
+} from "./data";
 //import getTokenDetails from "./jwt";
 //import Axios from "axios";
 
@@ -11,15 +18,21 @@ const Wallet = (props) => {
   const [totalBenefactors, setTotalBenefactors] = useState(0);
   const [pendingRequest, setPendingRequest] = useState(0);
   const [totalBeneficiary, setTotalBeneficiary] = useState(0);
+  const [benefactors, setBenefactors] = useState(null)
+  const [beneficiaries, setBeneficiaries] = useState(null)
+  const [showBeneficiariesTable, setShowBeneficiariesTable] = useState(false)
 
 
   // connect to back end and fetch all benefactors data for a user
-  /**const fetchAllbenefactors = async () => {
-    const token = await localStorage.getItem("UserToken");
+  const fetchAllbenefactors = async () => {
+    setBenefactors(benefactorsMockData)
+    setTotalBenefactors(benefactorsMockData.length)
+    setTotalBeneficiary(beneficiariesMockData.length)
+    //const token = await localStorage.getItem("UserToken");
     //const userInfo = await getTokenDetails(token);
-   // const userId = userInfo.payload[0].id;
+    // const userId = userInfo.payload[0].id;
 
-    await Axios.get()
+    /**await Axios.get()
       .then(async (res) => {
         const wallet = await res.data;
         if (wallet.data.length !== 0) {
@@ -29,19 +42,31 @@ const Wallet = (props) => {
       })
       .catch((err) => {
         window.alert("Hoops!!!.. Some error occured please try again, make sure you're connected to internet");
-      });
+      });*/
 
-  };*/
+  };
 
   useEffect(() => {
-    //fetchAllbenefactors();
+    fetchAllbenefactors();
   }, []);
 
   // Direct to add benefactors page
   const addBenefactorsHandler = () => {
-      // Note 
-  //  props.history.push("/dashboard/user/add/benefactor");
+    // Note 
+    //  props.history.push("/dashboard/user/add/benefactor");
   };
+
+  const displayBeneficiariesTable = () => {
+    setShowBeneficiariesTable(true);
+    setBeneficiaries(beneficiariesMockData)
+    setBenefactors(null)
+  }
+
+  const displayBenefactorsTable = () => {
+    setShowBeneficiariesTable(false);
+    setBeneficiaries(null)
+    setBenefactors(benefactorsMockData)
+  }
 
   return (
     <div className="Wallet">
@@ -50,7 +75,7 @@ const Wallet = (props) => {
           <div className="H1"> Wallet</div>
           personal
         </div>
-        <div className="addBenefactors-btn row1-col2">
+        <div className="action-btn row1-col2">
           <button onClick={addBenefactorsHandler} >Add Benefactors</button>
           <div></div>
           <button onClick={addBenefactorsHandler} >Add beneficiaries</button>
@@ -60,23 +85,26 @@ const Wallet = (props) => {
         <div className="walletDetails row2-spanned-across-two-columns">
           <div className="walletBalance">
             <div className="space-div"> Wallet Balance</div>
-            <div className="space-div">&#x20A6; {wallet} </div>
+            <div className="space-div">&#x20A6; 7000 </div>
           </div>
           <div className="pendingRequest">
             <div className="space-div"> Pending Request </div>
             <div className="space-div">{pendingRequest}</div>
           </div>
-          <div className="totalBenefactors">
+          <div className="totalBenefactors" onClick={displayBenefactorsTable}>
             <div className="space-div"> Total Benefactors</div>
             <div className="space-div">{totalBenefactors}</div>
           </div>
-          <div className="totalBeneficiary">
-            <div className="space-div"> Total Beneficiary</div>
+          <div className="totalBeneficiary" onClick={displayBeneficiariesTable}>
+            <div className="space-div" > Total Beneficiary</div>
             <div className="space-div"> {totalBeneficiary}</div>
           </div>
         </div>
-        <div className="walletTableWrapper row3-spanned-across-two-columns">
-          <WalletTable benefactors={wallet} />
+        <div className="TableWrapper" >
+          {showBeneficiariesTable ?
+            <BeneficiariesTable beneficiaries={beneficiaries} /> :
+            <BenefactorsTable benefactors={benefactors} />
+          }
         </div>
       </div>
     </div>
